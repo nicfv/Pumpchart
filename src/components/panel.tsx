47752ed135fafa93a63a8705b2a53927f0b3919e
Nicolas Ventura <ventura@lbl.gov>
2025-12-29 13:48:14 -0800

    use container

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { PumpchartOptions } from 'types';
 import { useTheme2 } from '@grafana/ui';
+import { Container } from './container';
 
 export const PumpchartPanel: React.FC<PanelProps<PumpchartOptions>> = (props) => {
     const isDarkTheme: boolean = useTheme2().isDark;
-    return <div>Hello! Dark theme is {isDarkTheme ? 'y' : 'n'}. Test is {props.options.test.toString()}.</div>;
+    const demo: HTMLParagraphElement = document.createElement('p');
+    demo.textContent = `Dark theme is: ${isDarkTheme}.`;
+    return <Container child={demo} />;
 };
