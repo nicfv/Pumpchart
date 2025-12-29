1c87abb75bcaadb648d868481faa867cb9a2c3f4
Nicolas Ventura <ventura@lbl.gov>
2025-12-29 13:14:07 -0800

    change panel name

---
+++ src/components/panel.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { PanelProps } from '@grafana/data';
+import { PumpchartOptions } from 'types';
+import { useTheme2 } from '@grafana/ui';
+
+export const PumpchartPanel: React.FC<PanelProps<PumpchartOptions>> = (props) => {
+    const isDarkTheme: boolean = useTheme2().isDark;
+    return <div>Hello! Dark theme is {isDarkTheme ? 'y' : 'n'}.</div>;
+};
