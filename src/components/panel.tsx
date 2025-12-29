8cb6d23ecc841d84410575442b91883502848cc6
Nicolas Ventura <ventura@lbl.gov>
2025-12-29 15:59:59 -0800

    plot data!!!

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -5,11 +5,13 @@ import { useTheme2 } from '@grafana/ui';
 import { Container } from './container';
 import { PanelDataErrorView } from '@grafana/runtime';
 import { Pumpchart } from 'psychart';
+import { format } from 'formatter';
 
 export const PumpchartPanel: React.FC<PanelProps<PumpchartOptions>> = (props) => {
   const isDarkTheme: boolean = useTheme2().isDark;
   let innerElement: HTMLElement;
   try {
+    const formatted = format(props.data.series);
     const pumpchart: Pumpchart = new Pumpchart({
       axisColor: isDarkTheme ? '#303030' : '#E0E0E0',
       colorizeBy: props.options.colorizeBy,
@@ -47,7 +49,16 @@ export const PumpchartPanel: React.FC<PanelProps<PumpchartOptions>> = (props) =>
         speed: props.options.units.speed,
       },
     });
-    // TODO: plot data
+    for (const t in formatted) {
+      const flow: number | undefined = formatted[t][props.options.series.flow];
+      const head: number | undefined = formatted[t][props.options.series.head];
+      const power: number | undefined = formatted[t][props.options.series.power];
+      const speed: number | undefined = formatted[t][props.options.series.speed];
+      if (typeof flow !== 'number' || typeof head !== 'number') {
+        continue;
+      }
+      pumpchart.plot({ flow: flow, head: head, power: power, speed: speed }, { radius: props.options.radius, timestamp: +t });
+    }
     innerElement = pumpchart.getElement();
   } catch (ex: any) {
     return <PanelDataErrorView panelId={props.id} data={props.data} message={`${ex}`} />;
