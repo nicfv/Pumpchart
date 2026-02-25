c990752c82d5327da760744307eaf8fa65613e4a
Nicolas Ventura <ventura@lbl.gov>
2026-02-25 14:36:25 -0800

    okay so i guess eslint wants me to use Array<> this time...

--- src/formatter.ts
+++ src/formatter.ts
@@ -3,7 +3,7 @@ import { DataFrame, DataFrameView, FieldType, outerJoinDataFrames } from '@grafa
 /**
  * Join all series by time into an array of series and return the time field series name.
  */
-export function format(series: DataFrame[]): [string, Record<string, number>[]] {
+export function format(series: DataFrame[]): [string, Array<Record<string, number>>] {
     const formatted: DataFrameView = new DataFrameView(outerJoinDataFrames({ frames: series })!);
     const timeField: string | undefined = formatted.dataFrame.fields.find(field => field.type === FieldType.time)?.name;
     if (!timeField) {
