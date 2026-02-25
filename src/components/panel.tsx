86c4e7539664df94a4f0013ed77438b1ac8fee3d
Nicolas Ventura <ventura@lbl.gov>
2026-02-25 14:30:05 -0800

    combine into 1 function

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -5,14 +5,13 @@ import { useTheme2 } from '@grafana/ui';
 import { Container } from './container';
 import { PanelDataErrorView } from '@grafana/runtime';
 import { Pumpchart } from 'psychart';
-import { format, getTimeField } from 'formatter';
+import { format } from 'formatter';
 
 export const PumpchartPanel: React.FC<PanelProps<PumpchartOptions>> = (props) => {
   const isDarkTheme: boolean = useTheme2().isDark;
   let innerElement: HTMLElement;
   try {
-    const formatted = format(props.data.series);
-    const timeField = getTimeField(formatted);
+    const [timeField, formatted] = format(props.data.series);
     const pumpchart: Pumpchart = new Pumpchart({
       axisColor: isDarkTheme ? '#303030' : '#E0E0E0',
       colorizeBy: props.options.colorizeBy,
@@ -46,8 +45,8 @@ export const PumpchartPanel: React.FC<PanelProps<PumpchartOptions>> = (props) =>
         speed: props.options.units.speed,
       },
     });
-    for (const pt of formatted.toArray()) {
-      const time: number | undefined = timeField ? pt[timeField] : undefined;
+    for (const pt of formatted) {
+      const time: number | undefined = pt[timeField];
       const flow: number | undefined = pt[props.options.series.flow];
       const head: number | undefined = pt[props.options.series.head];
       const power: number | undefined = pt[props.options.series.power];
--- src/formatter.ts
+++ src/formatter.ts
@@ -1,15 +1,13 @@
 import { DataFrame, DataFrameView, FieldType, outerJoinDataFrames } from '@grafana/data';
 
 /**
- * Join all series by time into a DataFrameView.
+ * Join all series by time into an array of series and return the time field series name.
  */
-export function format(series: DataFrame[]): DataFrameView {
-    return new DataFrameView(outerJoinDataFrames({ frames: series })!);
-}
-
-/**
- * Extract the time field series name from the formatted data.
- */
-export function getTimeField(formatted: DataFrameView): string | undefined {
-    return formatted.dataFrame.fields.find(field => field.type === FieldType.time)?.name;
+export function format(series: DataFrame[]): [string, Record<string, number>[]] {
+    const formatted: DataFrameView = new DataFrameView(outerJoinDataFrames({ frames: series })!);
+    const timeField: string | undefined = formatted.dataFrame.fields.find(field => field.type === FieldType.time)?.name;
+    if (!timeField) {
+        throw new Error('Data is missing a time field.');
+    }
+    return [timeField, formatted.toArray()];
 }
